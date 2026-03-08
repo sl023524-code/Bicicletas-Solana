0c59bf395f2269c61158360325141ad0bee87f95
sl023524-code <sl023524@gmail.com>
2026-03-08 10:33:28 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ declare_id!("Bici1111111111111111111111111111111111111");
 pub mod bikechain {
     use super::*;
 
+    // CREAR BICICLETA
     pub fn crear_bicicleta(
         ctx: Context<CrearBicicleta>,
         modelo: String,
@@ -20,6 +21,31 @@ pub mod bikechain {
         bicicleta.marca = marca;
         bicicleta.precio = precio;
         bicicleta.disponible = true;
+
+        Ok(())
+    }
+
+    // ACTUALIZAR BICICLETA
+    pub fn actualizar_bicicleta(
+        ctx: Context<ActualizarBicicleta>,
+        modelo: String,
+        marca: String,
+        precio: u64,
+    ) -> Result<()> {
+
+        let bicicleta = &mut ctx.accounts.bicicleta;
+
+        bicicleta.modelo = modelo;
+        bicicleta.marca = marca;
+        bicicleta.precio = precio;
+
+        Ok(())
+    }
+
+    // ELIMINAR BICICLETA
+    pub fn eliminar_bicicleta(
+        _ctx: Context<EliminarBicicleta>
+    ) -> Result<()> {
 
         Ok(())
     }
@@ -49,6 +75,44 @@ pub struct CrearBicicleta<'info> {
     pub usuario: Signer<'info>,
 
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ActualizarBicicleta<'info> {
+
+    #[account(
+        mut,
+        seeds = [
+            b"bicicleta",
+            bicicleta.modelo.as_bytes(),
+            usuario.key().as_ref()
+        ],
+        bump
+    )]
+
+    pub bicicleta: Account<'info, Bicicleta>,
+
+    pub usuario: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct EliminarBicicleta<'info> {
+
+    #[account(
+        mut,
+        close = usuario,
+        seeds = [
+            b"bicicleta",
+            bicicleta.modelo.as_bytes(),
+            usuario.key().as_ref()
+        ],
+        bump
+    )]
+
+    pub bicicleta: Account<'info, Bicicleta>,
+
+    #[account(mut)]
+    pub usuario: Signer<'info>,
 }
 
 #[account]
