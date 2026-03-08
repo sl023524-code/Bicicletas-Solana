f12e609c5a16b24e4c30b50d8122792ca8a61603
sl023524-code <sl023524@gmail.com>
2026-03-08 12:59:11 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ declare_id!("11111111111111111111111111111111");
 pub mod bici {
     use super::*;
 
+    // CREATE
     pub fn crear_bicicleta(
         ctx: Context<CrearBicicleta>,
         modelo: String,
@@ -22,6 +23,30 @@ pub mod bici {
 
         Ok(())
     }
+
+    // UPDATE
+    pub fn actualizar_bicicleta(
+        ctx: Context<ActualizarBicicleta>,
+        modelo: String,
+        marca: String,
+        precio: u64,
+    ) -> Result<()> {
+
+        let bicicleta = &mut ctx.accounts.bicicleta;
+
+        bicicleta.modelo = modelo;
+        bicicleta.marca = marca;
+        bicicleta.precio = precio;
+
+        Ok(())
+    }
+
+    // DELETE
+    pub fn eliminar_bicicleta(
+        _ctx: Context<EliminarBicicleta>
+    ) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -34,6 +59,25 @@ pub struct CrearBicicleta<'info> {
     pub usuario: Signer<'info>,
 
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ActualizarBicicleta<'info> {
+
+    #[account(mut)]
+    pub bicicleta: Account<'info, Bicicleta>,
+
+    pub usuario: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct EliminarBicicleta<'info> {
+
+    #[account(mut, close = usuario)]
+    pub bicicleta: Account<'info, Bicicleta>,
+
+    #[account(mut)]
+    pub usuario: Signer<'info>,
 }
 
 #[account]
