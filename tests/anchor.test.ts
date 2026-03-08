3c07c1b385d993c9db4646788ba7360fcfc8aaf0
sl023524-code <sl023524@gmail.com>
2026-03-08 12:55:11 -0600

    Update anchor.test.ts

--- tests/anchor.test.ts
+++ tests/anchor.test.ts
@@ -1,39 +1,27 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
 
-describe("bikechain", () => {
+describe("bici", () => {
 
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
-  const program = anchor.workspace.Bikechain as Program;
+  const program = anchor.workspace.Bici;
 
   it("Crear bicicleta", async () => {
 
-    const modelo = "Trek500";
-
-    const [bicicletaPDA] = anchor.web3.PublicKey.findProgramAddressSync(
-      [
-        Buffer.from("bicicleta"),
-        Buffer.from(modelo),
-        provider.wallet.publicKey.toBuffer(),
-      ],
-      program.programId
-    );
+    const bicicleta = anchor.web3.Keypair.generate();
 
     await program.methods
-      .crearBicicleta(modelo, "Trek", new anchor.BN(5000))
+      .crearBicicleta("Trek500", "Trek", new anchor.BN(5000))
       .accounts({
-        bicicleta: bicicletaPDA,
+        bicicleta: bicicleta.publicKey,
         usuario: provider.wallet.publicKey,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
+      .signers([bicicleta])
       .rpc();
 
-    const cuenta = await program.account.bicicleta.fetch(bicicletaPDA);
-
-    console.log("Bicicleta:", cuenta);
-
+    console.log("Bicicleta creada");
   });
 
 });
